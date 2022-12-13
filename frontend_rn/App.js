5d02c127683f82aee8c1f6b70f35bd5819381da0
hoangminhdh11 <hoangminhdh11@gmail.com>
2022-12-14 03:49:04 +0700

    update

--- frontend_rn/App.js
+++ frontend_rn/App.js
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import AppContext from './context/Context';
 import Navigator from './navigations/Navigator'
 
 export default function App() {
   return (
-    <AppContext>
       <Navigator/>
-    </AppContext>
   );
 }
 
